ffe184e3afd3b4f49729383772b25d3ec0adedbc
Ganesh <100681292+Ganeshmrxx@users.noreply.github.com>
2023-07-20 22:37:27 +0530

    Update index.py

--- index.py
+++ index.py
@@ -14,50 +14,17 @@ bot = Bot(TOKEN)
 
 
 def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
-                              f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
+   
     update.message.reply_text("👇 Enter Movie Name 👇")
 
 
-def find_movie(update, context):
-    search_results = update.message.reply_text("Processing...")
-    query = update.message.text
-    movies_list = search_movies(query)
-    if movies_list:
-        keyboards = []
-        for movie in movies_list:
-            keyboard = InlineKeyboardButton(movie["title"], callback_data=movie["id"])
-            keyboards.append([keyboard])
-        reply_markup = InlineKeyboardMarkup(keyboards)
-        search_results.edit_text('Search Results...', reply_markup=reply_markup)
-    else:
-        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
-
-
-def movie_result(update, context) -> None:
-    query = update.callback_query
-    s = get_movie(query.data)
-    response = requests.get(s["img"])
-    img = BytesIO(response.content)
-    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
-    link = ""
-    links = s["links"]
-    for i in links:
-        link += "🎬" + i + "\n" + links[i] + "\n\n"
-    caption = f"⚡ Fast Download Links :-\n\n{link}"
-    if len(caption) > 4095:
-        for x in range(0, len(caption), 4095):
-            query.message.reply_text(text=caption[x:x+4095])
-    else:
-        query.message.reply_text(text=caption)
 
 
 def setup():
     update_queue = Queue()
     dispatcher = Dispatcher(bot, update_queue, use_context=True)
     dispatcher.add_handler(CommandHandler('start', welcome))
-    dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
-    dispatcher.add_handler(CallbackQueryHandler(movie_result))
+    
     return dispatcher
 
 
@@ -66,7 +33,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello Wttorld!'
 
 
 @app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
@@ -80,6 +47,6 @@ def respond():
 def set_webhook():
     s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
     if s:
-        return "webhook setup ok"
+        return "webhook setuptt ok"
     else:
         return "webhook setup failed"
